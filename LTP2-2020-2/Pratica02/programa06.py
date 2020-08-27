78e75c54feba4afbbe2cf4f49d0816417c81dce7
thonyaw <39969260+thonyaw@users.noreply.github.com>
2020-08-27 11:00:57 -0300

    Create programa06.py

---
+++ LTP2-2020-2/Pratica02/programa06.py
@@ -0,0 +1,16 @@
+numero = int(input('Escreva um numero:'))
+
+resto = numero % 2
+
+if resto == 0:
+  print('Par')
+  print('Vencedor')
+  print('Não desistsa de seus objetivos')
+  print('Não desista de seus objetivos')
+  print('O mindset é a chave do sucesso')
+else:
+  print('Impar.')
+  print('Never desista dos seus dreams')
+
+
+print('Esse sempre aparece!')
