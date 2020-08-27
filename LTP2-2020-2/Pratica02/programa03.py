6c229f40ecf3bfa0d22d17b330ec94b03f62746a
thonyaw <39969260+thonyaw@users.noreply.github.com>
2020-08-27 10:36:05 -0300

    Create programa03.py

---
+++ LTP2-2020-2/Pratica02/programa03.py
@@ -0,0 +1,7 @@
+#Usuário fala dois numeros e calculamos a compile
+n1 = int(input('Escreva o primeiro número: '))
+n2 = int(input('Escreva o segundo número: '))
+
+media = (n1 + n2) / 2
+
+print('A média é: ', media)
