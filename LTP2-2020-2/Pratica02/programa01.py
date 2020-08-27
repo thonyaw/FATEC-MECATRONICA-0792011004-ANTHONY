f96e4bd6045b8367a936d8141922e8912b758f0d
thonyaw <39969260+thonyaw@users.noreply.github.com>
2020-08-27 10:21:43 -0300

    Create programa01.py

---
+++ LTP2-2020-2/Pratica02/programa01.py
@@ -0,0 +1,9 @@
+#Função para escrever na tela
+print('Olá mundo.')
+print('Bom dia')
+#Função para ler entrada
+nome = input('Escreva seu nome: ')
+'''
+Isso é um comentário em bloco
+'''
+print('Bom dia, ', nome, '!')
